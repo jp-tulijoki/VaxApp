5dcfd784445760a865775e0a3ffe8acb53059482
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2021-08-14 11:24:15 +0300

    add sequelize config to heroku

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,16 +1,27 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const db =
+const localDB =
   process.env.ENVIRONMENT === "test"
     ? process.env.TEST_DATABASE
     : process.env.DATABASE;
 
-const sequelize = new Sequelize(db, process.env.DB_USER, process.env.PASSWORD, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(localDB, process.env.DB_USER, process.env.PASSWORD, {
+      host: "localhost",
+      dialect: "postgres",
+      logging: false,
+    });
 
 try {
   sequelize.authenticate();
